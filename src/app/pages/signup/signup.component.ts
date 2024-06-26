dfa5b9678edaaf6caaa471cdf01aa9bfea8d6600
Ygor Portes <ygorportes3@gmail.com>
2024-06-26 19:54:21 -0300

    feat: add signup logic

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -43,8 +43,8 @@ export class SignupComponent {
   }
 
   submit() {
-    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
-    next: () => this.toastService.success("Login feito com sucesso!"),
+    this.loginService.signup(this.signupForm.value.name, this.signupForm.value.email, this.signupForm.value.password).subscribe({
+    next: () => this.toastService.success("Registro feito com sucesso!"),
     error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
     })
   }
